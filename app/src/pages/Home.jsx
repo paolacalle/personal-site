2588bac985f5e79a409ce5a2a04360d1ee9aafca
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2025-12-29 12:24:24 -0500

    components folder

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -3,9 +3,9 @@
 
 export const Home = () => {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/* Theme Toggle */}
-
+            
 
             {/* Background Effects */}
 
