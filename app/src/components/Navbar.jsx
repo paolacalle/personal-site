62079194f7728fd5dfd0f702dbc7cdf11920b4c3
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2025-12-30 15:15:09 -0500

    navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,9 +1,51 @@
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
+const navItems = [
+    {name: 'Home', href: '#hero'},
+    {name: 'About', href: '#about'},
+    {name: 'Skills', href: '#skills'},
+    {name: 'Projects', href: '#projects'},
+    {name: 'Contact', href: '#contact'},
+]
 
 export const Navbar = () => {
-    return (
-        <div> 
+    const [isScrolled, setIsScrolled] = useState(false);
 
-        </div>
+    useEffect(() => {
+        // the user scrolls the beyond 10px from top
+        const handleScroll = () => {
+            setIsScrolled(window.screenY > 10);
+        }; 
+
+        // add event listener to detect scroll
+        window.addEventListener('scroll', handleScroll);
+
+        // cleanup -- in case component unmounts -- prevent memory leaks
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    }, []); // empty dependency array to run only once on mount
+
+    // JSX for the Navbar component
+    // apply different styles based on isScrolled state
+    return (
+        <nav
+            className={cn(
+                "fixed w-full z-40 transition-all duration-300",
+                isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-md" : "py-5"
+            )}
+        > 
+            <div className="container flex items-center justify-between">
+                {/* Navigation items */}
+                <a className="text-xl font-bold text-primary flex items-center"> 
+                    <span className="relative z-10"> 
+                        <span className="text-glow text-foreground"> Paola Calle </span> Portfolio
+                    </span>
+                </a>
+            </div>
+
+        </nav>
     )
 };
