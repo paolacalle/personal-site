304e6fc65b707b3a87560efa3b4b1a460b348275
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-03-16 10:47:16 -0400

    resume HERF link

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const navItems = [
     {name: 'About', href: '#about'},
     {name: 'Skills', href: '#skills'},
     {name: 'Projects', href: '#projects'},
+    {name: 'Resume', href: '#resume'},
     {name: 'Contact', href: '#contact'},
 ]
 
