fa4494efcf356c08493934f96af01f60e5b02d24
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-03-27 12:13:40 -0400

    temp remove unused herf on nav

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -7,9 +7,10 @@ const navItems = [
     {name: 'Home', href: '#hero'},
     {name: 'About', href: '#about'},
     {name: 'Skills', href: '#skills'},
-    {name: 'Projects', href: '#projects'},
+    // {name: 'Projects', href: '#projects'},
+    {name: 'Journey', href: '#journey'},
     {name: 'Resume', href: '#resume'},
-    {name: 'Contact', href: '#contact'},
+    // {name: 'Contact', href: '#contact'},
 ]
 
 export const Navbar = () => {
