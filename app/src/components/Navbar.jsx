81cb3c7dfde3fc1e00abcbde933caa070e3dd147
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-03-27 13:07:55 -0400

    Revert "Journey component"

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -7,10 +7,9 @@ const navItems = [
     {name: 'Home', href: '#hero'},
     {name: 'About', href: '#about'},
     {name: 'Skills', href: '#skills'},
-    // {name: 'Projects', href: '#projects'},
-    {name: 'Journey', href: '#journey'},
+    {name: 'Projects', href: '#projects'},
     {name: 'Resume', href: '#resume'},
-    // {name: 'Contact', href: '#contact'},
+    {name: 'Contact', href: '#contact'},
 ]
 
 export const Navbar = () => {
